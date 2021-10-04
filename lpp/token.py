ebdcee308485bb2536e074c4f64fe4dceb03610b
Pegadro <pegadro.alvarado@gmail.com>
2021-10-04 13:41:44 -0500

    Lexer extension: conditionals, operations and booleans | Also challenge

--- lpp/token.py
+++ lpp/token.py
@@ -13,18 +13,30 @@ from typing import (
 class TokenType(Enum):
     ASSIGN = auto()
     COMMA = auto()
+    DIVISION = auto()
+    ELSE = auto() # sino
     EOF = auto() # Enf Of File
+    FALSE = auto()
     FUNCTION = auto()
+    GT = auto()
     IDENT = auto() # Identificador
+    IF = auto()
     ILLEGAL = auto() # Cuando un caracter no pertenece al lenguaje
     INT = auto()
     LBRACE = auto() # Llave izquierda {
     LET = auto() # Definición de variables
     LPAREN = auto() # Paréntesis izquierdo (
+    LT = auto() # menor que
+    MINUS = auto()
+    MULTIPLICATION = auto()
+    NEGATION = auto()
     PLUS = auto() # Suma
     RBRACE = auto() # Llave derecha 
+    RETURN = auto()
     RPAREN = auto() # Paréntesis derecho )
     SEMICOLON = auto() # PUnto y coma
+    TRUE = auto()
+
 
 
 class Token(NamedTuple):
@@ -39,8 +51,13 @@ def lookup_token_type(literal: str) -> TokenType:
 
     # Una variable keyword que es un diccionario que tiene como llaves strngs y como valores TokenType
     keywords: Dict[str, TokenType] = {
-        'variable': TokenType.LET,
+        'falso': TokenType.FALSE,
         'procedimiento': TokenType.FUNCTION,
+        'regresa': TokenType.RETURN,
+        'si': TokenType.IF,
+        'si_no': TokenType.ELSE,
+        'variable': TokenType.LET,
+        'verdadero': TokenType.TRUE
     }
 
     # Miramos si es una palabra reservada de nuestro lenguaje, si no lo es, entonces es un identificadir (un nombre de variable p.ej)
