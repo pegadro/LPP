4f756f2c95aacd7a60d8e4a23320890b963305cd
Pegadro <pegadro.alvarado@gmail.com>
2021-10-05 09:35:17 -0500

    program or main node parsing

--- lpp/ast.py
+++ lpp/ast.py
@@ -3,6 +3,8 @@ from abc import (
     abstractmethod,
 )
 
+from typing import List
+
 from lpp.token import Token
 
 class ASTNode(ABC):
@@ -35,3 +37,21 @@ class Expression(ASTNode):
     def token_literal(self) -> str:
         return self.token.literal
     
+
+class Program(ASTNode):
+
+    def __init__(self, statements: List[Statement]) -> None:
+        self.statements = statements
+
+    def token_literal(self) -> str:
+        if len(self.statements) > 0:
+            return self.statements[0].token_literal()
+        
+        return ''
+
+    def __str__(self) -> str:
+        out: List[str] = []
+        for statement in self.statements:
+            out.append(str(statement))
+
+        return ''.join(out)
